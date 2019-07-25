c5172264dcc9786a18ced6d30954e632047a9fab
BryanPatucci <bryanpatuccisilva2002@gmail.com>
2019-07-26 03:33:14 +0900

    Adicionando os cálculos de subtração, multiplicação e divisão á calculadora

--- www/components/acao.js
+++ www/components/acao.js
@@ -5,3 +5,24 @@ $(document).on("click", "#btnSomar", function(){
   var resultado = parseFloat(valor1) + parseFloat(valor2);
   $("#resultado").val(resultado);
 })
+
+$(document).on("click", "#btnSubtração", function(){
+  var valor1 = $("#valor1").val();
+  var valor2 = $("#valor2").val();
+  var resultado = parseFloat(valor1) - parseFloat(valor2);
+  $("#resultado").val(resultado);
+})
+
+$(document).on("click", "#btnMultiplicação", function(){
+  var valor1 = $("#valor1").val();
+  var valor2 = $("#valor2").val();
+  var resultado = parseFloat(valor1) * parseFloat(valor2);
+  $("#resultado").val(resultado);
+})
+
+$(document).on("click", "#btnDivisão", function(){
+  var valor1 = $("#valor1").val();
+  var valor2 = $("#valor2").val();
+  var resultado = parseFloat(valor1) / parseFloat(valor2);
+  $("#resultado").val(resultado);
+})
